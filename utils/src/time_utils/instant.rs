c43d39ed77dfba2a455aec27020698f560652e27
wadeud <161763388+wadeud@users.noreply.github.com>
2024-08-18 07:29:23 +0100

    Update instant.rs

--- utils/src/time_utils/instant.rs
+++ utils/src/time_utils/instant.rs
@@ -1,4 +1,4 @@
-//! This Instant measures time with high performance and high accuracy powered by TSC.
+//! This `Instant` measures time with high performance and high accuracy powered by TSC.
 use super::cycles;
 use std::time::Duration;
 
@@ -7,31 +7,29 @@ use std::time::Duration;
 pub struct Instant(u64);
 
 impl Instant {
-    /// Returns an instant corresponding to "now".
+    /// Returns an `Instant` corresponding to "now".
     #[inline]
-    pub fn now() -> Instant {
+    pub fn now() -> Self {
         Instant(cycles::rdtsc())
     }
 
-    /// Returns the amount of cpu cycles from another instant to this one,
-    /// or None if that instant is later than this one.
+    /// Returns the amount of CPU cycles from another `Instant` to this one,
+    /// or `None` if that `Instant` is later than this one.
     pub fn checked_cycles_since(&self, earlier: Instant) -> Option<u64> {
-        Some(self.0.checked_sub(earlier.0)?)
+        self.0.checked_sub(earlier.0)
     }
 
-    /// Returns the amount of nanos from another instant to this one,
-    /// or None if that instant is later than this one.
+    /// Returns the amount of nanoseconds from another `Instant` to this one,
+    /// or `None` if that `Instant` is later than this one.
     pub fn checked_nanos_since(&self, earlier: Instant) -> Option<f64> {
-        Some(cycles::convert_cycles_to_ns_f64(
-            self.0.checked_sub(earlier.0)?,
-        ))
+        self.checked_cycles_since(earlier)
+            .map(cycles::convert_cycles_to_ns_f64)
     }
 
-    /// Returns the amount of duration from another instant to this one,
-    /// or None if that instant is later than this one.
+    /// Returns the amount of `Duration` from another `Instant` to this one,
+    /// or `None` if that `Instant` is later than this one.
     pub fn checked_duration_since(&self, earlier: Instant) -> Option<Duration> {
-        Some(cycles::convert_cycles_to_duration(
-            self.0.checked_sub(earlier.0)?,
-        ))
+        self.checked_cycles_since(earlier)
+            .map(cycles::convert_cycles_to_duration)
     }
 }
